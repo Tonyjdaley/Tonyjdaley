c3389798e8dcebc9993894829f74350f85f9833e
Tonyjdaley <tonyjdaley@gmail.com>
2021-02-19 14:19:57 -0900

    update

--- lesson7/lesson7.js
+++ lesson7/lesson7.js
@@ -55,3 +55,31 @@ function windchill() {
     return f;
 }
 document.getElementById("chill").innerHTML = windchill();
+
+//Lazy Loads
+
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+
+if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
