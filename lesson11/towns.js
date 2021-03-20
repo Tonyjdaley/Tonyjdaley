cecfe4ad8427b30143de13cdca85bc434ebabf52
Tonyjdaley <tonyjdaley@gmail.com>
2021-03-20 12:16:54 -0800

    major update

--- lesson11/towns.js
+++ lesson11/towns.js
@@ -38,3 +38,27 @@ fetch(requestURL)
         
     }
   });
+   
+  // towns events
+
+  fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+     // temporary checking for valid response and data parsing
+    const townData = jsonObject['towns'];
+    const townName = document.getElementById('town-name').innerHTML;
+    const townFilter = townData.filter(x => x.name == townName);
+    const eventList = townFilter[0].events;
+    let events = document.createElement('section');
+
+    for (let j = 0; j<eventList.length; j++){
+        let p = document.createElement('p');
+        p.textContent = eventList[j];
+        events.appendChild(p);
+      }
+
+      document.querySelector('div.events').appendChild(events);
+    
+  });
