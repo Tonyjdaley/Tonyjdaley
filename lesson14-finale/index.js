5e1d51ae8e18505bd2a5c7d2bf942a812140d655
Tonyjdaley <tonyjdaley@gmail.com>
2021-04-08 01:00:07 -0800

    update

--- lesson14-finale/index.js
+++ lesson14-finale/index.js
@@ -6,7 +6,6 @@ hambutton.addEventListener('click', () => {mainnav.classList.toggle('t')}, false
 window.onresize = () => {if (window.innerWidth > 592) mainnav.classList.remove('t')};
 
 //DoW
-//dates
 function lastModified() {
     var d = new Date();
   
